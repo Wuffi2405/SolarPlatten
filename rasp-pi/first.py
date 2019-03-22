6a24fed5a12ad0dcf24cd4964e8af75fd39f1423
Wuffi2405 <wuffi2405@gmail.com>
2019-03-22 12:50:30 +0100

    python update

--- rasp-pi/first.py
+++ rasp-pi/first.py
@@ -1,49 +1,50 @@
 import serial				#Arduino Verbindung
-from datetime import date	#für das heutige Datum
 import os					#für Ordner Handling
 
 #arduino = serial.Serial('COM6', 9600, timeout=.1)
 
-#Referenzvariable für den heuteigen Tag
-today = str(date.today())
+#ZUM BEARBEITEN
+PATH = ""
+LENGTH = 80
 
+#andere Variablen
+today_old = 0
 #Überprüfung, ob Unterordner existieren
-#print(os.getcwd())
-if os.path.isdir("/tageslisten"):
+if os.path.isdir(PATH + "/tageslisten"):
 	print("Ordner existiert")
 else:
 	print("Bitte einen Ordner 'Tageslisten' erstellen")
-#	os.rmdir("/tageslisten")
-#else:
-#	try:  
-#		os.mkdir("\\tageslisten")
-#	except OSError:  
-#		print ("Creation of the directory %s failed" % "/tageslisten")
-#	else:  
-#		print ("Successfully created the directory %s" % "/tageslisten")
 	
 while True:
-
-	if today == str(date.today()):
-		print(today)
-	else:
-		today = str(date.today());
 	
-	data = arduino.readline()[:-2]
-	#data = ';08.03.2019 16:45:49;"0,00";"3,18";"0,00";"0,00";0;0;1;0000;0651;0000;0000;01;""'
+	#Daten für Speicherung preparieren
+		#data = arduino.readline()[:-2]
+	data = ',08.06.2019 16:45:49,"0,00","3,18","0,00","0,00",0,0,1,0000,0651,0000,0000,01,""'
+	x = data.split('"')
+	value = x[0].replace(',', '') + "," + x[1].replace(',', '.') + "," + x[3].replace(',', '.')
+	print(value)
+	
+	date = x[0].split(" ")
+	today = date[0].replace(",", "")
+	print(today)
+	
+	#if today_old == today:
+	#	print("selber Tag")
+	#else:
+	#	today_old = today
+	#	print("neuer Tag")
+		
 	if data:
-		f = open('messungen_roh.csv', 'a')
-		f.write('\n' + data.decode('utf-8'))
-		#f.write('\n' + data)
+		f = open(PATH + 'messungen_roh.csv', 'a')
+		#f.write(PATH + '\n' + value.decode('utf-8'))
+		f.write(PATH + '\n' + value)
 		f.close()
 		
-		f = open("tageslisten/"+today+".csv", 'a')
-		f.write('\n' + data.decode('utf-8'))
-		#f.write('\n' + data)
+		f = open(PATH + "tageslisten/"+today+".csv", 'a')
+		#f.write(PATH + '\n' + value.decode('utf-8'))
+		f.write(PATH + '\n' + value)
 		f.close()
 		
-		print(data)
-		#today = str(date.today())
-		#print(today)
+		#print(data)
 	
 	break
