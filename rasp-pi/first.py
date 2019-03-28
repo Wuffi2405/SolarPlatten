9bac0862d32238c210314a2e7a299fe95003b617
Wuffi2405 <wuffi2405@gmail.com>
2019-03-28 21:58:16 +0100

    raspi update

--- rasp-pi/first.py
+++ rasp-pi/first.py
@@ -1,48 +1,84 @@
 import serial				#Arduino Verbindung
 import os					#für Ordner Handling
 
-#arduino = serial.Serial('COM6', 9600, timeout=.1)
-
-#ZUM BEARBEITEN
+#
+#ZUM BEARBEITEN --- VARIABLEN
+#
 PATH = ""
 LENGTH = 80
+DEBUG = True;
 
+#
 #andere Variablen
+#
 today_old = 0
-#Überprüfung, ob Unterordner existieren
+
+#
+#NullPath durch aktuelles Verzeichnis ersetzen
+#
+if PATH == "":
+	PATH = os.path.dirname(os.path.abspath(__file__))
+
+#
+#Herstellung einer Verbindung zum Arduino
+#
+if DEBUG == False:
+	arduino = serial.Serial('COM6', 9600, timeout=.1)
+
+#
+#Ueberprüfung, ob Unterordner existieren
+#
 if os.path.isdir(PATH + "/tageslisten"):
-	print("Ordner existiert")
+	if DEBUG == True:
+		print("[INFO] Ordner '" + PATH + "/tageslisten' existiert")
 else:
-	print("Bitte einen Ordner 'Tageslisten' erstellen")
+	print("[ERROR] Ordner '" + PATH + "/tageslsiten' existiert nicht")
 	
 while True:
 	
+	#
 	#Daten für Speicherung preparieren
-		#data = arduino.readline()[:-2]
-	data = ',08.06.2019 16:45:49,"0,00","3,18","0,00","0,00",0,0,1,0000,0651,0000,0000,01,""'
+	#
+	if DEBUG == False:	
+		data = arduino.readline()[:-2]
+	else:
+		data = ',09.06.2019 16:45:49,"0,00","3,18","0,00","0,00",0,0,1,0000,0651,0000,0000,01,""'
+	
+	#
+	#Datenzeile wird geteilt
+	#wichtige Daten werden in Variable gespeichert
+	#
 	x = data.split('"')
 	value = x[0].replace(',', '') + "," + x[1].replace(',', '.') + "," + x[3].replace(',', '.')
-	print(value)
+	print("[INFO] Datenzeile: " + value)
 	
 	date = x[0].split(" ")
 	today = date[0].replace(",", "")
-	print(today)
-	
-	#if today_old == today:
-	#	print("selber Tag")
-	#else:
-	#	today_old = today
-	#	print("neuer Tag")
+	print("[LOL] heutiges Datum: " + today)
 		
 	if data:
-		f = open(PATH + 'messungen_roh.csv', 'a')
-		#f.write(PATH + '\n' + value.decode('utf-8'))
-		f.write(PATH + '\n' + value)
+		#
+		#rohe Daten >> messungen_roh.csv
+		#
+		f = open(PATH + '\messungen_roh.csv', 'a')
+		#f.write('\n' + value.decode('utf-8'))
+		f.write('\n' + data)
 		f.close()
 		
-		f = open(PATH + "tageslisten/"+today+".csv", 'a')
-		#f.write(PATH + '\n' + value.decode('utf-8'))
-		f.write(PATH + '\n' + value)
+		#
+		#optimierte Daten >> messungen.csv
+		#
+		f = open(PATH + '\messungen.csv', 'a')
+		#f.write('\n' + value.decode('utf-8'))
+		f.write('\n' + value)
+		f.close()
+		
+		#
+		#tagesspezif. Daten >> tageslisten
+		#
+		f = open(PATH + "/tageslisten/"+today+".csv", 'a')
+		#f.write('\n' + value.decode('utf-8'))
+		f.write('\n' + value)
 		f.close()
 		
 		#print(data)
